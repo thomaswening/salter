3b4a86502d6d5b63a3009012d002eac46ccd6cd1
Thomas Wening <thomaswening94@gmail.com>
2025-02-21 16:04:37 +0100

    Add new encryption framework with key management.
    - Introduce a new encryption framework for managing encryption keys and performing encryption and decryption operations.
    - `SecretManager` provides an abstract base for loading secrets from and saving to environment variables or files.
    - The class `KeyManager` extends `SecretManager` for key/IV handling. This class manages encryption keys and initialization vectors, supporting loading and saving keys and IVs from environment variables and files.
    - `KeyIvEncryptor` uses `KeyManager` for key/IV management and provides methods for encrypting and decrypting data using symmetric encryption algorithms and utilizes a `KeyManager` to manage encryption keys and initialization vectors.
    - Move all this into core class library.

---
+++ Salter/Salter.Core/Encryption/IEncryptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salter.Core.Encryption;
+
+internal interface IEncryptor
+{
+    byte[] Decrypt(byte[] data);
+    Task<byte[]> DecryptAsync(byte[] data);
+    byte[] Encrypt(byte[] data);
+    Task<byte[]> EncryptAsync(byte[] data);
+}
---
+++ Salter/Salter.Core/Encryption/KeyManager.cs
@@ -0,0 +1,79 @@
+﻿namespace Salter.Encryption;
+
+/// <summary>
+/// The <see cref="KeyManager"/> class extends the <see cref="SecretManager"/> class to manage encryption keys and initialization vectors.
+/// It supports loading and saving keys and initialization vectors from environment variables and files.
+/// </summary>
+internal class KeyManager : SecretManager
+{
+    public class Options
+    {
+        public SourceType SourceType { get; set; }
+        public string KeySource { get; set; } = string.Empty;
+        public string InitializationVectorSource { get; set; } = string.Empty;
+    }
+
+    private readonly Options options;
+
+    public KeyManager(Options options) : base(options.SourceType)
+    {
+        ValidateSource(options.KeySource, options.SourceType, nameof(options.KeySource));
+        ValidateSource(options.InitializationVectorSource, options.SourceType, nameof(options.InitializationVectorSource));
+
+        this.options = options;
+    }
+
+    public (byte[] key, byte[] initializationVector) Load()
+    {
+        return options.SourceType switch
+        {
+            SourceType.Environment => (LoadFromEnvironment(options.KeySource), LoadFromEnvironment(options.InitializationVectorSource)),
+            SourceType.File => (LoadFromFile(options.KeySource), LoadFromFile(options.InitializationVectorSource)),
+            _ => throw new InvalidOperationException("Invalid key source type.")
+        };
+    }
+
+    public async Task<(byte[] key, byte[] initializationVector)> LoadAsync()
+    {
+        return options.SourceType switch
+        {
+            SourceType.Environment => (LoadFromEnvironment(options.KeySource), LoadFromEnvironment(options.InitializationVectorSource)),
+            SourceType.File => (await LoadFromFileAsync(options.KeySource), await LoadFromFileAsync(options.InitializationVectorSource)),
+            _ => throw new InvalidOperationException("Invalid key source type.")
+        };
+    }
+
+    public void Save(byte[] key, byte[] initializationVector)
+    {
+        switch (options.SourceType)
+        {
+            case SourceType.Environment:
+                SaveToEnvironment(options.KeySource, key);
+                SaveToEnvironment(options.InitializationVectorSource, initializationVector);
+                break;
+            case SourceType.File:
+                SaveToFile(options.KeySource, key);
+                SaveToFile(options.InitializationVectorSource, initializationVector);
+                break;
+            default:
+                throw new InvalidOperationException("Invalid key source type.");
+        }
+    }
+
+    public async Task SaveAsync(byte[] key, byte[] initializationVector)
+    {
+        switch (options.SourceType)
+        {
+            case SourceType.Environment:
+                SaveToEnvironment(options.KeySource, key);
+                SaveToEnvironment(options.InitializationVectorSource, initializationVector);
+                break;
+            case SourceType.File:
+                await SaveToFileAsync(options.KeySource, key);
+                await SaveToFileAsync(options.InitializationVectorSource, initializationVector);
+                break;
+            default:
+                throw new InvalidOperationException("Invalid key source type.");
+        }
+    }
+}
