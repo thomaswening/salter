84034c7a8eb0f27b7687a5e7b600fc2e315bba5d
Thomas Wening <thomaswening94@gmail.com>
2025-02-27 17:41:27 +0100

    Add project for persistence layer.
    - The abstract `Repository` implements an in-memory cache and defines contracts for CRUD operations. Since this persistence layer should store sensitive information, it is injected with an `IEncryptor` that should encrypt the data before persisting it and decrypt it when loading it.
    - Since it's to be expected that sometimes records cannot be found, there is the custom type `RecordNotFoundException`. It inherits from the more generic `RepositoryException` that allows clients to distinguish any exceptions thrown by the repo from the rest. Implementations of the abstract `Repository` can then implement further custom exceptions by inheriting from `RepositoryException`.
    - The `JsonRepository` represents a JSON file based repo. It implements the CRUD operations defined in `Repository` by persisting the records to the JSON file and also executing the equivalent operation on the cache. If anything goes wrong, the operation on the cache is rolled back and the underlying exception is wrapped in a `RepositoryException` with the appropriate message.
    - The repositories take objects of type `Entity`, which represent business objects, and serialize them into objects of `DataTransferObject`, which are mapped to an specific entity type via the type parameter `T`. These DTOs are essentially just data containers. Vice versa, the data read from the JSON file is deserialized into DTOs which then validate themselves via `DataTransferObject.Validate()` in order to protect the inner state of the application. If the validation fails, this method throws a `ValidationException`. Ideally, the validation logic should also be separated from the DTOs and centralized but since this is just a demo/study project, I don't want to bother myself with this now.
    - The objects are mapped to one another in the repository using a `Mapper` which is a generic type that takes the type arguments from the business model and the DTO and then defines methods to map one into the other and vice versa. For the DTO -> BO direction, the DTO is validated.
    - Also make `IEncryptor` available in the project by making it public.

---
+++ Salter/Salter.Persistence/Mapper.cs
@@ -0,0 +1,38 @@
+﻿using Salter.Core.DataManagement;
+
+namespace Salter.Persistence;
+
+/// <summary>
+/// Maps between models and data transfer objects.
+/// </summary>
+/// <typeparam name="TModel">The type of the model.</typeparam>
+/// <typeparam name="TDto">The type of the data transfer object.</typeparam>
+internal abstract class Mapper<TModel, TDto>
+    where TDto : IDataTransferObject<TModel>
+    where TModel : Entity
+{
+    public abstract TDto MapToDataTransferObject(TModel model);
+
+    public TModel ToModel(TDto dto)
+    {
+        var validationError = dto.Validate();
+        if (validationError is not null)
+        {
+            throw validationError;
+        }
+
+        return MapToModel(dto);
+    }
+
+    internal List<TDto> MapToDataTransferObjects(IEnumerable<TModel> models)
+    {
+        return models.Select(MapToDataTransferObject).ToList();
+    }
+
+    internal List<TModel> MapToModels(IEnumerable<TDto> dtos)
+    {
+        return dtos.Select(ToModel).ToList();
+    }
+
+    protected abstract TModel MapToModel(TDto dto);
+}
