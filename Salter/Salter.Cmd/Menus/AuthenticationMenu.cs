9e65decc786dd542f06f29a5f10d02794ced0b7e
Thomas Wening <thomaswening94@gmail.com>
2025-03-03 19:14:09 +0100

    Set up menus for authentication and user actions.
    - `AuthenaticationMenu` lets the users log in with their credential or register as new users.
    - `UserActionsMenu` lets a logged in user execute various actions on their own account or even others', depending on their user role: change username or password, create new user, make user admin, view all registered users, delete their own account or another account.
    - Constants like the application name and application paths are stored in `AppConstants`. Since they are not likely to be modified, I haven't bothered to implement a JSON repo for the app settings.
    - Fatal errors are logged into the app directory and a new log is created for each new instance of the app with an iterator appended to the filename.
    - Change to `AuthenticationService`: Inject instance of `PasswordHasher` and expose as property, so this can be used to hash passwords. This is better than creating a new instance outside of the auth service.

---
+++ Salter/Salter.Cmd/Menus/AuthenticationMenu.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Salter.Core.UserManagement;
+
+namespace Salter.Cmd.Menus;
+
+/// <summary>
+/// This menu allows the user to authenticate or register.
+/// </summary>
+internal class AuthenticationMenu : Menu
+{
+    private readonly UserActionsMenu _userActionsMenu;
+    public override string Title => "Authentication";
+
+    public AuthenticationMenu(AuthenticationService authenticationService, UserActionsMenu userActionsMenu) : base(authenticationService)
+    {
+        _userActionsMenu = userActionsMenu;
+        HasGoBack = false;
+    }
+
+    private async Task RegisterNewUserAsync()
+    {
+        if (!ConsoleInputHelper.GetUserInput(
+            "Username",
+            _authService.ValidateUsername,
+            out var username,
+            "Please enter your username"))
+        {
+            ConsoleInputHelper.PromptContinue();
+            return;
+        }
+
+        if (!ConsoleInputHelper.GetSecretUserInput(
+            "Password",
+            AuthenticationService.ValidatePassword,
+            out var password,
+            "Please enter your password"))
+        {
+            ConsoleInputHelper.PromptContinue();
+            return;
+        }
+
+        Console.WriteLine();
+
+        try
+        {
+            await _authService.RegisterAsync(username, password).ConfigureAwait(false);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Could not register user. Please try again.");
+            Console.WriteLine(e.Message);
+            ConsoleInputHelper.PromptContinue();
+            return;
+        }
+
+        Console.WriteLine("You have successfully registered!");
+        ConsoleInputHelper.PromptContinue();
+    }
+
+    private async Task AuthenticateUserAsync()
+    {
+        Console.WriteLine("Please enter your username and password to login.");
+
+        var isAuthenticated = false;
+
+        while (!isAuthenticated)
+        {
+            if (!ConsoleInputHelper.GetUserInput(
+                "Username",
+                p => !string.IsNullOrWhiteSpace(p),
+                out var username,
+                "Please enter your username"))
+            {
+                ConsoleInputHelper.PromptContinue();
+                return;
+            }
+
+            if (!ConsoleInputHelper.GetSecretUserInput(
+                "Password",
+                p => p.Length > 0,
+                out var password,
+                "Please enter your password"))
+            {
+                ConsoleInputHelper.PromptContinue();
+                return;
+            }
+
+            Console.WriteLine();
+
+            try
+            {
+                isAuthenticated = await _authService.AuthenticateAsync(username, password);
+                if (!isAuthenticated)
+                {
+                    Console.WriteLine("Invalid credentials. Please try again.");
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not authenticate user. Please try again.");
+                Console.WriteLine(ExceptionHelper.UnpackException(e));
+                ConsoleInputHelper.PromptContinue();
+                return;
+            }
+        }
+
+        Console.WriteLine("You have successfully logged in!");
+        ConsoleInputHelper.PromptContinue();
+    }
+
+    private async Task RegisterAndAuthenticateUserAsync()
+    {
+        await RegisterNewUserAsync();
+
+        Console.WriteLine("Please login to continue.");
+        Console.WriteLine();
+
+        await AuthenticateUserAsync();
+    }
+
+    protected override List<MenuItem> CreateMenuItems()
+    {
+        return
+        [
+            new("Authenticate", AuthenticateUserAsync, subMenu: _userActionsMenu),
+            new("Register", RegisterAndAuthenticateUserAsync, subMenu: _userActionsMenu),
+        ];
+    }
+
+    protected override void OnExit() => _authService.Logout();
+    protected override void OnGoBack() => _authService.Logout();
+}
---
+++ Salter/Salter.Cmd/Program.cs
@@ -0,0 +1,77 @@
+﻿using Salter.Core.UserManagement;
+using Salter.Core.Encryption;
+using Salter.Persistence;
+using Salter.Encryption;
+using System.Security.Cryptography;
+using Salter.Core;
+using Salter.Cmd.Menus;
+
+namespace Salter.Cmd;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            JsonRepository<User, UserDto> repository = InitializeRepository();
+            var passwordHasher = new PasswordHasher();
+            var userManager = new UserManager(repository, passwordHasher);
+            await userManager.InitializeAsync();
+
+            var authService = new AuthenticationService(userManager, passwordHasher);
+            var userActionsMenu = new UserActionsMenu(authService, userManager);
+            var mainMenu = new AuthenticationMenu(authService, userActionsMenu);
+            var menuNavigator = new MenuNavigator(mainMenu);
+            var menuDisplay = new MenuDisplay(menuNavigator);
+
+            menuDisplay.Start();
+        }
+        catch (OperationCanceledException)
+        {
+            // Input cancellation, just exit
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("A critical error has occurred and the application will exit now.");
+            Console.WriteLine();
+            Console.WriteLine("== Details ==");
+            Console.WriteLine();
+            Console.WriteLine(ExceptionHelper.UnpackException(e));
+
+            SaveErrorLog(e);
+        }
+
+        ConsoleInputHelper.PromptContinue();
+    }
+
+    private static JsonRepository<User, UserDto> InitializeRepository()
+    {
+        var keyManOptions = new KeyManager.Options()
+        {
+            SourceType = SecretManager.SourceType.Environment,
+            KeySource = AppConstants.KeyEnvironmentVariable,
+            InitializationVectorSource = AppConstants.IvEnvironmentVariable
+        };
+
+        var keyManager = new KeyManager(keyManOptions);
+        var encryptor = new KeyIvEncryptor(keyManager, Aes.Create);
+        var mapper = new UserMapper();
+        var repository = new JsonRepository<User, UserDto>(AppConstants.UserRepositoryUri, encryptor, mapper);
+        return repository;
+    }
+
+    private static void SaveErrorLog(Exception e)
+    {
+        // Get next iteration suffix for error log file
+        var iteration = 0;
+        while (File.Exists(AppConstants.GetErrorLogPath(iteration)))
+        {
+            iteration++;
+        }
+
+        var errorLogPath = AppConstants.GetErrorLogPath(iteration);
+        File.WriteAllText(errorLogPath, ExceptionHelper.UnpackException(e));
+    }
+}
